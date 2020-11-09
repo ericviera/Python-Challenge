fa8c32b5767a9bebbb8b43f7f2623023f396edd8
Eric Viera <ericviera@Erics-MBP.home>
2020-11-09 18:42:16 -0500

    6th commit

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -6,6 +6,17 @@ csvpath = os.path.join('Resources', 'budget_data.csv')
 # variables
 total_months = 0
 net_prof_loss = 0
+count = 0
+avg_change = 0
+high = 0
+low = 0
+month = []
+monthly_change = []
+amount = []
+change = []
+best_date = ""
+worst_date = ""
+
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
@@ -17,12 +28,31 @@ with open(csvpath) as csvfile:
         # looping through the first index of the file to find the net profit/loss
         net_prof_loss += int(row[1])
 
+        if count == 0:
+            count += 1
+            newamount = int(row[1])
+            amount.append(int(row[1]))
+            month.append(str(row[0]))
+
+        else:
+            amount.append(int(row[1]))
+            month.append(str(row[0]))
+            change.append(int(int(row[1])-newamount))
+            monthly_change.append(str(row[0]))
+            newamount = int(row[1])
+
+    avg_change = round(sum(change) / len(monthly_change), 2)
+    high = max(change)
+    best_date = change.index(high)
+    low = min(change)
+    worst_date = change.index(low)
+
 
 print("---------------------------------")
 print("Financial Analysis")
 print("---------------------------------")
 print(f"Total Months:  {total_months}")
 print(f"Total:  ${net_prof_loss}")
-#print(f"Average Change:  ${avg_change}")
-#print(f"Greatest Increase in Profits:  {best_date} (${high})")
-#print(f"Greatest Decrease in Losses:  {worst_date} (${low})")
+print(f"Average Change:  ${avg_change}")
+print(f"Greatest Increase in Profits:  {monthly_change[best_date]} (${high})")
+print(f"Greatest Decrease in Losses:  {monthly_change[worst_date]} (${low})")
