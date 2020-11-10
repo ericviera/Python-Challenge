37d514df340bfaadea1f764d8a2a17dcdefcc466
Eric Viera <ericviera@Erics-MBP.home>
2020-11-09 20:01:12 -0500

    pypoll

---
+++ PyPoll/Main.py
@@ -0,0 +1,21 @@
+import os
+import csv
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+csvpath = os.path.join('Resources', 'election_data.csv')
+
+
+total_votes = 0
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
+
+    for row in csvreader:
+        votes += 1
+
+
+print("-------------------------")
+print("Election Results")
+print("-------------------------")
+print(f"Total Votes: {votes}")
+print("-------------------------")
