725b772af3126fb878e1b3fd79f8e0bd0f030be8
Eric Viera <ericviera@Erics-MBP.home>
2020-11-09 20:54:26 -0500

    8th commit

--- PyPoll/Main.py
+++ PyPoll/Main.py
@@ -3,18 +3,34 @@ import csv
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 csvpath = os.path.join('Resources', 'election_data.csv')
 
-votes = 0
+votes_total = 0
+
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
 
     for row in csvreader:
-        votes += 1
+        votes_total += 1
+        candidate_in = (row[2])
 
 
 print("-------------------------")
 print("Election Results")
 print("-------------------------")
-print(f"Total Votes: {votes}")
+print(f"Total Votes: {votes_total}")
 print("-------------------------")
+print("-------------------------")
+#print(f"winner: {winner}")
+print("-------------------------")
+
+
+txtfile = open("Election_Results.txt", 'w')
+txtfile.write("-------------------------\n")
+txtfile.write("Election Results\n")
+txtfile.write("-------------------------\n")
+txtfile.write(f"Total Votes: {votes_total}\n")
+txtfile.write("-------------------------\n")
+txtfile.write("-------------------------\n")
+#txtfile.write(f"winner: {winner}\n")
+txtfile.write("-------------------------\n")
